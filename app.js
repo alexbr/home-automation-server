d3d4900904f1f1b4ab47638b5ee3675899033886
Alex Rodriguez <alexbr@gmail.com>
2017-04-07 13:16:24 -0700

    change static dir

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /static
+//app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 if (settings.auth) {
    app.all('*', (req, res, next) => {
